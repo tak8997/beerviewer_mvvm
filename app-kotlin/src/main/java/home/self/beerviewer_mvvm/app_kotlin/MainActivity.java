9c894e46d069e804a47eabfbc2cca9766902367b
DESKTOP-TMK1FU4\Administrator <lbt@bridge.co>
2018-05-10 17:40:06 +0900

    add kotlin project

---
+++ app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/MainActivity.java
@@ -0,0 +1,13 @@
+package home.self.beerviewer_mvvm.app_kotlin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

c6c7353620b63e57f97c307c4e5d7975cf30838f
Tak <tak8997@nate.com>
2018-05-12 18:06:02 +0900

    update gradle, convert java to kotlin

---
+++ app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package home.self.beerviewer_mvvm.app_kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

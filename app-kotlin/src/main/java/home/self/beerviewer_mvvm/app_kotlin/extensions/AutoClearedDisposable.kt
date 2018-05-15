0cee8b22bef560c302e09b2c95d0f9de6fb52e98
DESKTOP-TMK1FU4\Administrator <lbt@bridge.co>
2018-05-15 10:35:14 +0900

    update gradle, add lifecycleobserver class

---
+++ app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/extensions/AutoClearedDisposable.kt
@@ -0,0 +1,34 @@
+package home.self.beerviewer_mvvm.app_kotlin.extensions
+
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.OnLifecycleEvent
+import android.support.v7.app.AppCompatActivity
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+class AutoClearedDisposable(
+        private val lifeycycleOwner : AppCompatActivity,
+        private val clearOnStop : Boolean = true,
+        private val compositeDisposable: CompositeDisposable = CompositeDisposable())
+    : LifecycleObserver {
+
+    fun add(disposable: Disposable) {
+        check(lifeycycleOwner.lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED))
+        compositeDisposable.add(disposable)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun cleanUp() {
+        if (!clearOnStop && !lifeycycleOwner.isFinishing)
+            return
+        compositeDisposable.clear()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun detachSelf() {
+        compositeDisposable.clear()
+        lifeycycleOwner.lifecycle.removeObserver(this)
+    }
+}

6cd6a79d3a966b0dcdcb9a7041869c65bd8e6718
Tak <tak8997@nate.com>
2018-05-17 10:43:56 +0900

    update beers view

--- app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/view/beersview/BeersViewActivity.kt
+++ app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/view/beersview/BeersViewActivity.kt
@@ -15,6 +15,7 @@ import home.self.beerviewer_mvvm.app_kotlin.data.model.BeerModel
 import home.self.beerviewer_mvvm.app_kotlin.rx.lifecycle.AutoClearedDisposable
 import home.self.beerviewer_mvvm.app_kotlin.rx.schedulers.BaseSchedulerProvider
 import home.self.beerviewer_mvvm.app_kotlin.rx.schedulers.SchedulerProvider
+import home.self.beerviewer_mvvm.app_kotlin.view.beerdetail.BeerDetailActivity
 import kotlinx.android.synthetic.main.activity_beers_view.*
 import org.jetbrains.anko.startActivity
 import javax.inject.Inject
@@ -24,7 +25,7 @@ class BeersViewActivity : DaggerAppCompatActivity(), SwipyRefreshLayout.OnRefres
     val disposables = AutoClearedDisposable(this)
     val viewDisposables = AutoClearedDisposable(this, false)
 
-    @Inject lateinit var adapter: BeersAdapter
+    @Inject lateinit var beersAdapter: BeersAdapter
     @Inject lateinit var viewModelFactory : BeersViewModelFactory
 
     lateinit var viewModel : BeersViewModel
@@ -50,11 +51,11 @@ class BeersViewActivity : DaggerAppCompatActivity(), SwipyRefreshLayout.OnRefres
     }
 
     private fun getBeers(direction: SwipyRefreshLayoutDirection) {
-        viewDisposables.add(viewModel.getBeers(pageStart, perPage, direction)
+        viewDisposables.add(viewModel.getBeers(pageStart++, perPage, direction)
                 .subscribeOn(schedulerProvider.io())
                 .observeOn(schedulerProvider.ui())
                 .subscribe {beers ->
-                    adapter.addItems(beers)
+                    beersAdapter.addItems(beers)
                 })
     }
 
@@ -62,7 +63,7 @@ class BeersViewActivity : DaggerAppCompatActivity(), SwipyRefreshLayout.OnRefres
         setSupportActionBar(app_bar)
 
         with(beer_recyler) {
-            adapter = this@BeersViewActivity.adapter
+            adapter = this@BeersViewActivity.beersAdapter
             layoutManager = LinearLayoutManager(this@BeersViewActivity)
         }
 
@@ -76,7 +77,7 @@ class BeersViewActivity : DaggerAppCompatActivity(), SwipyRefreshLayout.OnRefres
 
     override fun onRefresh(direction: SwipyRefreshLayoutDirection) {
         handler.postDelayed({
-            getBeers(direction)
+//            getBeers(direction)
 
             refresh_layout.isRefreshing = false
         }, Constant.REFRESH_DELAY)

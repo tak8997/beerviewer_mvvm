12d26c2918d21128dfa8079163f02aec376e4f5b
byungtak <>
2018-10-22 00:49:54 +0900

    update splashactivity

--- app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/view/splash/SplashActivity.kt
+++ app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/view/splash/SplashActivity.kt
@@ -1,22 +1,18 @@
 package home.self.beerviewer_mvvm.app_kotlin.view.splash
 
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import home.self.beerviewer_mvvm.app_kotlin.BaseActivity
 import home.self.beerviewer_mvvm.app_kotlin.R
+import home.self.beerviewer_mvvm.app_kotlin.extensions.observe
 import home.self.beerviewer_mvvm.app_kotlin.rx.lifecycle.AutoClearedDisposable
-import home.self.beerviewer_mvvm.app_kotlin.rx.schedulers.BaseSchedulerProvider
 import home.self.beerviewer_mvvm.app_kotlin.view.beersview.BeersViewActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 import org.jetbrains.anko.longToast
 import org.jetbrains.anko.startActivity
-import javax.inject.Inject
 
 internal class SplashActivity : BaseActivity<SplashViewModel.ViewModel>() {
-
-    @Inject lateinit var schedulerProvider: BaseSchedulerProvider
-
-    private val disposables = AutoClearedDisposable(this)
 
     override fun getLayoutRes(): Int = R.layout.activity_splash
 
@@ -25,27 +21,22 @@ internal class SplashActivity : BaseActivity<SplashViewModel.ViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        lifecycle.addObserver(disposables)
-
-        disposables.add(viewModel.message
-                .observeOn(schedulerProvider.ui())
-                .subscribe {message ->
-                    longToast(message)
-                })
-
-        disposables.add(viewModel.isSplashLoading
-                .observeOn(schedulerProvider.ui())
-                .subscribe {it ->
-                    if (it == false)
-                        startActivity<BeersViewActivity>().apply { finish() }
-                })
+        subscribeLooknFeel()
 
         showSplashAnimation()
     }
 
-    override fun onResume() {
-        super.onResume()
-        disposables.add(viewModel.getBeers())
+    private fun subscribeLooknFeel() {
+        observe(viewModel.outputs.errorMessage(), ::handleMessage)
+        observe(viewModel.outputs.isSplashLoadingEnd(), ::handleSplashLoading)
+    }
+
+    private fun handleSplashLoading(isLoading: Boolean) {
+        startActivity<BeersViewActivity>()
+    }
+
+    private fun handleMessage(message: String) {
+        longToast(message)
     }
 
     private fun showSplashAnimation() {
@@ -54,6 +45,5 @@ internal class SplashActivity : BaseActivity<SplashViewModel.ViewModel>() {
                 .load(R.raw.beer_splash)
                 .into(beer_animation)
     }
-
 
 }
--- app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/view/splash/SplashViewModel.kt
+++ app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/view/splash/SplashViewModel.kt
@@ -1,35 +1,52 @@
 package home.self.beerviewer_mvvm.app_kotlin.view.splash
 
+import android.arch.lifecycle.MutableLiveData
 import home.self.beerviewer_mvvm.app_kotlin.BaseViewModel
-import home.self.beerviewer_mvvm.app_kotlin.BeerViewerApplication
-import home.self.beerviewer_mvvm.app_kotlin.R
-import home.self.beerviewer_mvvm.app_kotlin.data.source.BeerRepository
+import home.self.beerviewer_mvvm.app_kotlin.data.model.BeerModel
 import home.self.beerviewer_mvvm.app_kotlin.data.source.BeerRepositoryApi
 import home.self.beerviewer_mvvm.app_kotlin.di.qualifier.App
 import home.self.beerviewer_mvvm.app_kotlin.rx.schedulers.BaseSchedulerProvider
+import io.reactivex.Single
 import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.subjects.BehaviorSubject
 import javax.inject.Inject
 
 internal interface SplashViewModel {
 
+    interface Inputs
+
+    interface Outputs {
+        fun isSplashLoadingEnd(): MutableLiveData<Boolean>
+        fun errorMessage(): MutableLiveData<String>
+    }
+
     class ViewModel @Inject constructor(
-            val repostory : BeerRepositoryApi,
-            val schedulerProvider: BaseSchedulerProvider
+            @App private val repostory : BeerRepositoryApi
 
-    ): BaseViewModel()  {
+    ): BaseViewModel(), Inputs, Outputs {
 
-        val message : BehaviorSubject<String> = BehaviorSubject.create()
-        val isSplashLoading : BehaviorSubject<Boolean> = BehaviorSubject.create()
+        val inputs = this
+        val outputs = this
 
-        fun getBeers() : Disposable
-                = repostory.getBeers()
-                .doOnSubscribe { isSplashLoading.onNext(true) }
-                .doOnTerminate { isSplashLoading.onNext(false) }
-                .subscribeOn(schedulerProvider.io())
-                .subscribe({it ->
-                    if (it.isEmpty())
-                        message.onNext(BeerViewerApplication.instance.getString(R.string.no_data))
-                })
+        private val message = MutableLiveData<String>()
+        private val splashLoading = MutableLiveData<Boolean>()
+
+        init {
+            compositeDisposable.addAll(
+                    repostory
+                            .getBeers(1, 25)
+                            .doOnError {
+                                message.postValue("알 수 없는 에러입니다. 다시 시도해주세요.")
+                            }
+                            .subscribeBy {
+                                splashLoading.postValue(true)
+                            }
+            )
+        }
+
+        override fun isSplashLoadingEnd(): MutableLiveData<Boolean> = splashLoading
+
+        override fun errorMessage(): MutableLiveData<String> = message
     }
 }

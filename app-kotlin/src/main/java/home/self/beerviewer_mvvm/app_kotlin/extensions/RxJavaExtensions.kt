573f894cf29dd75d6bf00c5cecbc1cefb5ef8256
byungtak <>
2018-10-28 00:11:34 +0900

    add rx extension about async compose

--- app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/extensions/RxJavaExtensions.kt
+++ app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/extensions/RxJavaExtensions.kt
@@ -1,14 +1,22 @@
 package home.self.beerviewer_mvvm.app_kotlin.extensions
 
 import home.self.beerviewer_mvvm.app_kotlin.rx.schedulers.SchedulerProvider
-import io.reactivex.Completable
-import io.reactivex.Scheduler
+import io.reactivex.*
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import java.lang.IllegalArgumentException
 
-fun runOnIoThread(schedulerIO : Scheduler, func : ()->Unit) : Disposable {
+fun runOnIoThread(schedulerIO : Scheduler, func : () -> Unit) : Disposable {
     if (Schedulers.io() == schedulerIO)
         return Completable.fromCallable(func).subscribeOn(schedulerIO).subscribe()
     else
-        throw IllegalAccessException("must be a IO scheduler")
+        throw IllegalArgumentException("must be a IO scheduler")
+}
+
+fun <T> applyObservableAsync(): ObservableTransformer<T, T> {
+    return ObservableTransformer { observable ->
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
 }

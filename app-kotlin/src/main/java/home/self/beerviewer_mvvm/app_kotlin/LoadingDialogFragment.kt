743a1174dab9db9dc7aa6e190d7b3dab1086a680
byungtak <>
2018-10-27 21:08:44 +0900

    update loading dialog fragment view

--- app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/LoadingDialogFragment.kt
+++ app-kotlin/src/main/java/home/self/beerviewer_mvvm/app_kotlin/LoadingDialogFragment.kt
@@ -3,23 +3,14 @@ package home.self.beerviewer_mvvm.app_kotlin
 
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 internal class LoadingDialogFragment : DialogFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_loading_dialog, container, false)
-
-        return view
-    }
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+            = inflater.inflate(R.layout.fragment_loading_dialog, container, false)
 
 }
